cda25b68565510538ac242b852fc030ba9cff521
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-05 01:04:59 +0300

    Finally the code works

--- BigO.py
+++ BigO.py
@@ -1,18 +1,32 @@
 #BigO
 
-def linear_search (list, target):
+def binary_search(list, target):
     low = 0
     high = len(list) - 1
 
     while low <= high:
         mid = (low + high) // 2
         guess = list [mid]
-
-        if guess == target:
-            return True
+        
+        
+        if guess  == target:
+            
+            return  True 
+    
+    
         elif guess < target:
             low = mid + 1
-        else :
+
+        else:
+            
             high = mid - 1
+            
     return False
 
+
+my_list = [1, 3, 5, 7, 9]
+
+print (binary_search(my_list, 3))
+print (binary_search(my_list, -1))
+print (binary_search(my_list, 9))
+print (binary_search(my_list, 4))
