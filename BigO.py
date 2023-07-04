18732848305366e61c0aaa4485c5a0fd8b9f589e
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-04 23:59:58 +0300

    b

--- BigO.py
+++ BigO.py
@@ -1,1 +1,18 @@
 #BigO
+
+def linear_search (1st, target):
+    low = 0
+    high = len(1st) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = 1st [mid]
+
+        if guess == target;
+            return True
+        elif guess < target:
+            low = mid + 1
+        else :
+            high = mid - 1
+    return False
+
