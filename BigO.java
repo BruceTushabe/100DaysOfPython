c1eef280bb9270bece030b105c7101d18d3cac0c
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-05 01:28:56 +0300

    This code is for Java

---
+++ BigO.java
@@ -0,0 +1,38 @@
+public class BigO {
+
+    public static boolean binarySearch (int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low  <= high){
+            int mid = low + (high - low)/ 2;
+
+            int guess = arr[mid];
+
+            if (guess == target) {
+                return  true;
+
+
+            } else if (guess < target) {
+                low = mid + 1;
+
+
+            } else {
+                high  = mid - 1;
+
+            }
+
+        return false
+
+        }
+
+    public static void main (String[] args){
+        int[] myArray = {1,3,5,7,9};
+
+            System.out.println(binarySearch(myArray, 3));   // Output: true
+            System.out.println(binarySearch(myArray, -1));  // Output: false
+            System.out.println(binarySearch(myArray, 9));   // Output: true
+            System.out.println(binarySearch(myArray, 4));   // Output: false
+    }    
+}
