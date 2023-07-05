3947eadccac43aefe8c1b90231ca84e0b9bc655b
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-06 01:28:09 +0300

    This a linkedlist for Java

---
+++ linked.java
@@ -0,0 +1,37 @@
+// Java Program to demonstrate linked list
+
+import java.util.*;
+
+
+
+public class linked {
+
+    // driver code 
+
+    public static void main(String[] args){
+
+        // Creating the object of the class linked list
+
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // Adding elements to the linked list
+
+        ll.add("A");
+        ll.add("B");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2, "E");
+
+
+        System.out.println(ll);
+
+        ll.remove("B");
+        ll.remove(3);
+        ll.removeFirst();
+        ll.removeLast();
+  
+        System.out.println(ll);
+    }
+}
+
