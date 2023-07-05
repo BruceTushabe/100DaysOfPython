9a79f543b3bed61a7af9491b918008ac902167a6
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-05 23:42:17 +0300

    Added linked list

---
+++ linked.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                print (current.data, end="")
+                current = current.next
+
+# Create a linked list and append elements
+
+linked_list =  LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+
+# Display the linkedlist
+
+linked_list.display() # type: ignore
