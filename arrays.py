69d85dff5821b1aad87b737861d32744cdae1d3a
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-07 13:06:00 +0300

    Let's make this happen - Chatbot in python created

---
+++ arrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#Create a numpy array
+
+arr = np.array([1,2,3,4,5])
+
+#Perform operations on the array
+
+print("Array:", arr)
+print("Sum:", np.sum(arr))
+print("Mean:", np.mean(arr))
+print("Max:", np.max(arr))
+print("Squared values:", np.square(arr))
+
+#Wow , I like numpy
+
+
--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,44 @@
-import numpy as np
-from keras.models import Squential
-from keras.layers import Dense, Activation, Dropout
-from keras.optmizers import SGD
-import random
-
-
 import nltk
 
+#Dowlnlaod required NLTK data
+
+nltk.download ('punkt')
+nltk.download ('averaged_perceptron_tagger')
+nltk.download ('wordnet')
+
+#Define functions for processing user input and generating responses
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+# Tokenise user input
+
+def tokenize(text):
+    return word_tokenize(text.lower())
+
+# Lemmatize words
+
+def lemmatize(tokens):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+# Generate response
+
+def generate_response (user_input):
+
+    return "This is a simple response."
+
+while True:
+
+    user_input = input("User: ")
+    tokens = tokenize(user_input)
+    lemmas = lemmatize(tokens)
+    response = generate_response(lemmas)
+
+    if any(keyword in lemmas for keyword in [" Uganda", "Kampala", "africa"]):
+        response = "Uganda is the greatest country in the world"
+    print("Chatbot: " + response)
 
 
 
