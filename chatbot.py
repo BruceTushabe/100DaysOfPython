c54720f254a90e11a5f548f74c882a5d79539afa
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-08 19:46:26 +0300

    Added functionalities to the chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,44 +1,86 @@
 import nltk
 
+from nltk.chat.util import Chat, reflections
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 #Dowlnlaod required NLTK data
 
 nltk.download ('punkt')
-nltk.download ('averaged_perceptron_tagger')
-nltk.download ('wordnet')
+nltk.download ('vader_lexicon')
 
-#Define functions for processing user input and generating responses
+sia = SentimentIntensityAnalyzer()
 
-from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
+# Define chatbot patterns and responses
+chatbot_pairs = [
+    [
+        r"hi|hey|hello",
+        ["Hello!", "Hi there!", "Greetings!"]
+    ],
+    [
+        r"what is your name?",
+        ["You can call me ChatBot.", "I'm ChatBot, nice to meet you!"]
+    ],
+    [
+        r"how are you ?",
+        ["I'm good, thanks! How about you?", "I'm doing well, thank you!"]
+    ],
+    [
+        r"(.*) your name ?",
+        ["You can call me ChatBot.", "I'm ChatBot, nice to meet you!"]
+    ],
+    [
+        r"(.*) (age|old) ?",
+        ["I'm a chatbot, I don't have an age."]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ["I'm a chatbot, I exist in the digital world."]
+    ],
+    [
+        r"(.*) (created|made) you ?",
+        ["I was created by OpenAI using Python and NLTK."]
+    ],
+    [
+        r"(.*)",
+        ["Sorry, I didn't quite understand that.", "Can you please rephrase that?"]
+    ]
+]
 
-# Tokenise user input
 
-def tokenize(text):
-    return word_tokenize(text.lower())
+# Create a chat instance
 
-# Lemmatize words
+chatbot = Chat(chatbot_pairs, reflections)
 
-def lemmatize(tokens):
-    lemmatizer = WordNetLemmatizer()
-    return [lemmatizer.lemmatize(token) for token in tokens]
+def generate_response(user_input):
+    sentiment = sia.polarity_scores(user_input)["compound"]
 
-# Generate response
+    if sentiment > 0.3:
+        response = "I am glad to hear that!"
 
-def generate_response (user_input):
+    elif sentiment < -0.3:
+        response = "I'm sorry to hear that. Is there anything I can do to help?"
+    else:
+        response = chatbot.respond(user_input)
 
-    return "This is a simple response."
+    return response
 
+# Start the chat 
+
+print ("ChatBot: Hello, how can I assist you today? ")
 while True:
-
     user_input = input("User: ")
-    tokens = tokenize(user_input)
-    lemmas = lemmatize(tokens)
-    response = generate_response(lemmas)
+    if user_input.lower() == "bye":
+        print("ChatBot: Goodbye. Have a great day!")
+        break
 
-    if any(keyword in lemmas for keyword in [" Uganda", "Kampala", "africa"]):
-        response = "Uganda is the greatest country in the world"
-    print("Chatbot: " + response)
+    else:
+        try:
 
+            response = generate_response(user_input)
 
+        except Exception as e:
+            print ("ChatBot: Oops! An error occured. Please try again.")
+
+            continue
+        print("ChatBot: ", response)
 
