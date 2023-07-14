8e1e66671b49f27a1d737bfef3229c27c6d36ce8
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-14 22:20:39 +0300

    The Joureny of Greatness

---
+++ dataScience.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+users = pd.read_table('ml-1m/users.dat', sep='::', header= None, names = unames)
+
+
+rnames = ['user_id', 'movied_id', 'rating', 'timestamp']
+ratings = pd.read_table('ml-1m/ratings.dat', sep= '::', header= None, names=rnames)
+
+
+mnames = [ 'movie_id', 'title', 'genres']
+movies = pd.read_table('ml-1m/movies.dat', sep='::', header= None, names= mnames)
+
