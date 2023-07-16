bcc3e8697d910b64e36c2b73eb4370af9aec86d7
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-16 23:26:51 +0300

    Adding matrices

---
+++ matrixSubtract.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+matrix_a = np.array([[1,1,1],
+                     [1,1,1],
+                     [1,1,2]])
+
+
+# create a matrix
+
+matrix_b = np.array([[1,3,1],
+                    [1,3,1],
+                    [1,3,8]]),
+
+# Add matrices
+
+np.add(matrix_a, matrix_b)
+
