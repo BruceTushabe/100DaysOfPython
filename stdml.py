35334fe7651080ca6f38d2dd056e602468a06aa8
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-18 00:51:56 +0300

    Learning new habits

---
+++ stdml.py
@@ -0,0 +1,20 @@
+# Lets deal with standard deviation 
+
+import numpy as np
+
+speed = [86,87,88,86,87,85,86]
+
+
+x = np.std(speed)
+
+y = np.var(speed)
+
+
+print(x, y)
+
+
+# Lets deal with variance
+
+
+
+
