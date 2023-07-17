81ec17ffa2e68dcab37573d4569938264534f88a
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-18 00:35:13 +0300

    I keep doing the needful

---
+++ mlxplore.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import stats
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = np.mean(speed)
+y = np.median(speed)
+z = stats.mode(speed)
+
+
+print (x, y, z)
