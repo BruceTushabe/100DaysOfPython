351f359d4b25f0fceac9e1840122f70ff0b73bea
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-18 12:23:33 +0300

    HEY

--- stdml.py
+++ stdml.py
@@ -1,7 +1,7 @@
 # Lets deal with standard deviation 
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 speed = [86,87,88,86,87,85,86]
 
 
@@ -9,11 +9,16 @@ x = np.std(speed)
 
 y = np.var(speed)
 
+# Creating random datasets
 
-print(x, y)
+x = np.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x,100)
+plt.show()
+
+print(x)
 
 
-# Lets deal with variance
 
 
 
