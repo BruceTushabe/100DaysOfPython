26331599547b26fa931ce915076a39f5840bf0b9
Bruce Tushabe <brucetushabe@gmail.com>
2023-07-20 00:15:11 +0300

    Many interesting things

--- kisoboka.py
+++ kisoboka.py
@@ -3,16 +3,16 @@ import numpy as np
 
 # Data 
 
-years = np.arange(2020, 2026)
-total_market = np.array([50000, 52000, 54000, 56000, 58000, 60000])
-kisoboka_market = np.array([10000, 12000, 14000, 16000, 18000, 20000])
+years = np.arange(2023, 2026)
+total_market = np.array([50000, 51000, 53000, 54000, 55000, 5000])
+kisoboka_market = np.array([3000, 4500, 6000, 7500, 9000, 10000])
 
 # Plot
 
 plt.plot(years, total_market, label='Total Market')
-plt.plot(years, kisoboka_market, label ='Kisoboka Market')
+plt.plot(years, kisoboka_market, label ='Kisoboka Africa Market')
 plt.xlabel('Year')
-plt.ylabel('Number of Households')
+plt.ylabel('Number of Households in Lwengo')
 plt.title('Kisoboka Africa Target Market and Market Share')
 plt.legend()
 plt.show()
