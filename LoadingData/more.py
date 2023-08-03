46d2c5b5e26e7fb9d0a3e6e6ecfed3fc68df3f21
Bruce Tushabe <brucetushabe@gmail.com>
2023-08-04 01:34:28 +0300

    More dealing with data using Numpy) git commit

---
+++ LoadingData/more.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+
+
+data = [50,50,47,97,49,3,53,42,26,74,82,62,37,15,70,27,36,35,48,52,63,64]
+print(data)
+
+# Define an array of study hours
+study_hours = [10.0,11.5,9.0,16.0,9.25,1.0,11.5,9.0,8.5,14.5,15.5,
+               13.75,9.0,8.0,15.5,8.0,9.0,6.0,10.0,12.0,12.5,12.0]
+
+grades = np.array(data)
+
+student_data = np.array([study_hours, grades])
+
+
+shep = student_data.shape
+
+print(shep)
